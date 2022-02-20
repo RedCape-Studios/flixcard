6cc608895c9b8a3c9645ffa1a3fbe59d55eebe0f
dhzdhd <ragas5432100@gmail.com>
2022-02-20 15:26:55 +0530

    Some changes

--- lib/widgets/tabs.dart
+++ lib/widgets/tabs.dart
@@ -1,4 +1,3 @@
-import 'package:black_tortoise/models/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,24 +8,20 @@ class ContentTabWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeModel>(
-      builder {
-        return Padding(
-          padding: EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
-          child: Ink(
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: InkWell(
-              borderRadius: BorderRadius.circular(15),
-              onTap: () => print('hello'),
-              child: child,
-            ),
-          ),
-        );
-      }
+    return Padding(
+      padding: EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
+      child: Ink(
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(15),
+          onTap: () => print('hello'),
+          child: child,
+        ),
+      ),
     );
   }
 }
